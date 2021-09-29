cc9f23dcb02d3208a31aa76651f2f8e6c0fef499
rustydd <bangyuli@buffalo.edu>
2021-09-29 16:23:34 +0800

    Update sorting.lua

--- sorting.lua
+++ sorting.lua
@@ -4,6 +4,9 @@ local animals = {
     {name = "cow", size = 30}
 }
 
+-- local mixTable = {"Gizmo", "xeno", 23, "Apple", "cruise", 46, 5, "binary", 99, 7, 11, 13}
+local mixTable = {"bronze", "Gizmo", "Bar", "almighty", "Tesla", "foo", "xeno", "care", "Apple", "cruise", "binary"}
+
 local key = "size"
 -- sort animals by size descending
 table.sort(animals, function(a,b) return a[key] > b[key] end)
@@ -13,3 +16,8 @@ print()
 -- sort animals by size ascending
 table.sort(animals, function(a,b) return a[key] < b[key] end)
 for i,v in ipairs(animals) do print(v.name) end
+
+print()
+
+table.sort(mixTable)
+for i,v in ipairs(mixTable) do print(v) end
